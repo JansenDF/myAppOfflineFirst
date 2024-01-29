c386bcf9e52f9e453e45391f33fa3e44b62d1969
Jansen Lima <jansen.lima.df@gmail.com>
2024-01-29 10:13:17 -0300

    Implementação parcial da sincronização de usuario no banco de dados online.

--- src/components/CadastroUsuario/index.tsx
+++ src/components/CadastroUsuario/index.tsx
@@ -12,11 +12,13 @@ import {
   View,
 } from 'react-native';
 
-import {database} from '../../database';
 import {User} from '../../database/model/UserModel';
 import mySync from '../../database/syncronize';
+import {useDatabase} from '@nozbe/watermelondb/react';
+import {Q} from '@nozbe/watermelondb';
 
 export default function CadastroUsuario() {
+  const database = useDatabase();
   const [name, setName] = useState('');
   const [matricula, setMatricula] = useState('');
   const [email, setEmail] = useState('');
@@ -24,34 +26,48 @@ export default function CadastroUsuario() {
   const [profile, setProfile] = useState('');
   const [users, setUsers] = useState<any>(null);
   const [loadingButton, setLoadingButton] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [idUpdate, setIdUpdate] = useState('');
 
   async function handleCreateUser() {
-    setLoadingButton(true);
-    await database.write(async () => {
-      await database
-        .get<User>('user')
-        .create(user => {
-          (user.name = name),
-            (user.matricula = matricula),
-            (user.email = email),
-            (user.password = password),
-            (user.profile_code = profile);
-        })
-        .then(() => {
-          Alert.alert('Sucesso!', 'Usuário criado com sucesso.');
-          setLoadingButton(false);
-          setName('');
-          setMatricula('');
-          setEmail('');
-          setPassword('');
-          setProfile('');
-          getUsers();
-        })
-        .catch(e => {
-          Alert.alert('Erro!', `${e}`);
-          setLoadingButton(false);
+    // setLoadingButton(true);
+    if (update) {
+      await database.write(async () => {
+        const updateUser = await database.get<User>('user').find(idUpdate);
+        await updateUser.update(() => {
+          updateUser.name = name;
         });
-    });
+        Alert.alert('Sucesso!', 'Usuário atualizado com sucesso.');
+      });
+      setLoadingButton(false);
+      getUsers();
+    } else {
+      await database.write(async () => {
+        await database
+          .get<User>('user')
+          .create(user => {
+            (user.name = name),
+              (user.matricula = matricula),
+              (user.email = email),
+              (user.password = password),
+              (user.profile_code = profile);
+          })
+          .then(() => {
+            Alert.alert('Sucesso!', 'Usuário criado com sucesso.');
+            setLoadingButton(false);
+            setName('');
+            setMatricula('');
+            setEmail('');
+            setPassword('');
+            setProfile('');
+            getUsers();
+          })
+          .catch(e => {
+            Alert.alert('Erro!', `${e}`);
+            setLoadingButton(false);
+          });
+      });
+    }
   }
 
   function handleReset() {
@@ -64,6 +80,7 @@ export default function CadastroUsuario() {
           setEmail('');
           setPassword('');
           setProfile('');
+          setUpdate(false);
         },
       },
       {
@@ -73,8 +90,30 @@ export default function CadastroUsuario() {
     ]);
   }
 
-  function handleSync() {
-    mySync();
+  const handleDelete = async (id: string) => {
+    await database
+      .write(async () => {
+        const user = await database.get('user').find(id);
+        await user.markAsDeleted();
+      })
+      .then(() => Alert.alert('Sucesso!', 'Usuário deletado com sucesso.'))
+      .catch(() => Alert.alert('Erro!', 'Não foi possível deletar o usuário.'))
+      .finally(() => getUsers());
+  };
+
+  const handleUpdate = async (user: User) => {
+    setName(user.name);
+    setMatricula(user.matricula);
+    setEmail(user.email);
+    setPassword(user.password);
+    setProfile(user.profile_code);
+    setUpdate(true);
+    setIdUpdate(user.id);
+  };
+
+  async function handleSync() {
+    await mySync();
+    await getUsers();
   }
 
   async function getUsers() {
@@ -121,11 +160,19 @@ export default function CadastroUsuario() {
         />
         <View style={styles.button}>
           <Button title="Limpar" onPress={handleReset} color="#EE0" />
-          <Button
-            title="Salvar"
-            onPress={handleCreateUser}
-            disabled={loadingButton}
-          />
+          {update ? (
+            <Button
+              title="Atualizar"
+              onPress={handleCreateUser}
+              disabled={loadingButton}
+            />
+          ) : (
+            <Button
+              title="Salvar"
+              onPress={handleCreateUser}
+              disabled={loadingButton}
+            />
+          )}
           <Button title="Sincronizar" onPress={handleSync} color="#F00" />
         </View>
         {users == null ? (
@@ -135,10 +182,25 @@ export default function CadastroUsuario() {
         ) : (
           users.map((user: User, index: any) => (
             <View key={index} style={styles.userList}>
-              <Text>Nome: {user.name}</Text>
-              <Text>Matrícula: {user.matricula}</Text>
-              <Text>Email: {user.email}</Text>
-              <Text>Profile_code: {user.profile_code}</Text>
+              <View>
+                <Text>ID: {user.id}</Text>
+                <Text>Nome: {user.name}</Text>
+                <Text>Matrícula: {user.matricula}</Text>
+                <Text>Email: {user.email}</Text>
+                <Text>Profile_code: {user.profile_code}</Text>
+              </View>
+              <View style={{position: 'absolute', alignSelf: 'flex-end'}}>
+                <Text onPress={() => handleDelete(user.id)}>Deletar</Text>
+              </View>
+              <View
+                style={{
+                  position: 'absolute',
+                  alignSelf: 'flex-end',
+                  paddingTop: 55,
+                }}
+              >
+                <Text onPress={() => handleUpdate(user)}>Editar</Text>
+              </View>
             </View>
           ))
         )}
--- src/database/syncronize/index.ts
+++ src/database/syncronize/index.ts
@@ -6,34 +6,21 @@ const mySync = async () => {
   await synchronize({
     database,
     pullChanges: async ({ lastPulledAt, schemaVersion, migration }) => {
-      console.log('Pull')
-      console.log(lastPulledAt)
-      console.log(schemaVersion)
-      console.log(migration)
-      console.log(database.collections)
+      console.log("PullChanges...")
       const urlParams = `last_pulled_at=${lastPulledAt}&schema_version=${schemaVersion}&migration=${encodeURIComponent(
         JSON.stringify(migration),
       )}`
-      await fetch('http://192.168.1.14:5000/user/')
-      console.log('Prrre-Response')
       const response = await fetch(`http://192.168.1.14:5000/sync?${urlParams}`)
+      if (!response.ok) {
+        throw new Error(await response.text())
+      }
       const res = response.json()
       const {changes, timestamp } = await res
-      console.log({changes, timestamp})
-
-      // console.log('resp ->',response)
-      // console.log('resp_headers ->',response.headers)
-      // console.log('resp_json -> ',response.json())
-      // const data = response.json()
-      // console.log("data ->", data)
-      // if (!response.ok) {
-      //   throw new Error(await response.text())
-      // }
-      // const { changes, timestamp } = await response.json()
+      console.log("Changes e Timestamp do Backend", {changes, timestamp})
       return { changes, timestamp }
     },
     pushChanges: async ({ changes, lastPulledAt }) => {
-      console.log('entrei no post!')
+      console.log("PushChanges...")
       const response = await fetch(`http://192.168.1.14:5000/sync?last_pulled_at=${lastPulledAt}`, {
         method: 'POST',
         body: JSON.stringify(changes),
@@ -44,7 +31,7 @@ const mySync = async () => {
     },
     migrationsEnabledAtVersion: 1,
   })
-  console.warn('FinalMySync')
+  console.warn('Sincronização realizada com sucesso!')
 }
 
 export default mySync
